d162e65972c9b8844a4ba9be43ec51a5efb6039f
wallax <wallax@126.com>
2016-05-07 01:05:08 +0800

    20160507

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 'use strict';
 
-module.exports = require('./lib/aliez.js');
+var http = require('http'),
+	assert = require('assert'),
+	EventEmitter = require('events');
+
+var midwares = [];
+var aliez = function(cb){
+	assert('function' == typeof cb, 'Invalid callback');
+	return function(req, res){
+		assert(req instanceof http.IncomingMessage &&
+			res instanceof http.ServerResponse,
+			'Invalid argument');
+		var e = new EventEmitter();
+		var count = 0;
+		e.on('done', function(){
+			count++;
+			if(count == midwares.length){
+				e.emit('finish', req, res);
+			}
+		}).on('finish', cb);
+		
+		if(midwares.length > 0){
+			for(var i = 0; i < midwares.length; i++){
+				midwares[i].call(this, req, res, function(){
+					e.emit('done');
+				});
+			}
+		}else{
+			e.emit('finish');
+		}
+	}
+}
+
+aliez.use = function(cb){
+	assert('function' == typeof cb, 'Invalid midware');
+	midwares.push(cb);
+	return aliez;
+}
+
+module.exports = aliez;
